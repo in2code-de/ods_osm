aea5e56c28769749805c93afecf664cc0d989125
Alex Kellner <alexander.kellner@in2code.de>
2020-03-11 13:55:36 +0100

    [BUGFIX] Add missing extension key for addPlugin()

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,9 @@
 <?php
-use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-ExtensionManagementUtility::addPlugin(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
     [
         'LLL:EXT:ods_osm/locallang_db.xml:tt_content.list_type_pi1',
         'ods_osm_pi1'
     ],
-    'list_type'
+    'list_type',
+    'ods_osm'
 );
