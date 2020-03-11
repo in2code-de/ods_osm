2fa62c5abab71f59db05b541c8ac64ef74853336
Alex Kellner <alexander.kellner@in2code.de>
2020-03-11 12:12:41 +0100

    [TASK] Update dependencies for TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,23 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'OpenStreetMap',
 	'description' => 'Add an interactive OpenStreetMap map to your website. Can also show other OpenLayers compatible maps.',
 	'author' => 'Robert Heel',
 	'author_email' => 'typo3@bobosch.de',
 	'category' => 'plugin',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
+	'constraints' => [
+		'depends' => [
+			'typo3' => '6.2.0-9.5.99',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
 			'ods_osm_cal' => '',
 			'ods_osm_tt_address' => '',
-		),
-	),
+		],
+	],
 	'createDirs' => 'uploads/tx_odsosm/map',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'version' => '2.2.0',
-);
-?>
+];
